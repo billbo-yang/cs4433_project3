d171596539a7799f7f2163cc3d34bafa427576d7
Billbo-Swaggins <coolbillyang@gmail.com>
2022-02-23 00:17:52 -0500

    adding initial comments and some code

--- p1_query1.py
+++ p1_query1.py
@@ -1,17 +1,27 @@
 import sys
- 
+
 from pyspark import SparkContext, SparkConf
- 
-if __name__ == "__main__":
-	
-	# create Spark context with necessary configuration
-	sc = SparkContext("local","PySpark Word Count Exmaple")
-	
-	# read data from text file and split each line into words
-	words = sc.textFile("PEOPLE.csv").flatMap(lambda line: line.split(","))
-	
-	# count the occurrence of each word
-	wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
-	
-	# save the counts to output
-	wordCounts.saveAsTextFile("temp/")
+
+def map_points(point_attributes):
+    id = point_attributes[0]
+    x_coord = point_attributes[1]
+    y_coord = point_attributes[2]
+    
+
+
+# create Spark context with necessary configuration
+sc = SparkContext("local", "PySpark Word Count Exmaple")
+
+# read infected points from file and store in memory
+
+
+# read data from text file and split each line into its normal point attributes
+points = sc.textFile("PEOPLE.csv").flatMap(lambda line: line.split(","))
+
+# for each normal point, check to see if its in range of any infected point
+point_map = points.map(
+        lambda attributes: map_points(attributes)
+    )
+
+# save the counts to output
+# wordCounts.saveAsTextFile("temp/")
