a821372cf4597c1c32a9998f0b84a986e42ff7d1
Billbo-Swaggins <coolbillyang@gmail.com>
2022-02-23 16:04:32 -0500

    query1 v1 done

--- p1_query1.py
+++ p1_query1.py
@@ -1,27 +1,64 @@
-import sys
+import math
 
 from pyspark import SparkContext, SparkConf
 
-def map_points(point_attributes):
-    id = point_attributes[0]
-    x_coord = point_attributes[1]
-    y_coord = point_attributes[2]
-    
+def map_points(infected_list, line):
+    output = ""
 
+    point_attributes = line.split(",")
+    point_id = int(point_attributes[0])
+    point_x = int(point_attributes[1])
+    point_y = int(point_attributes[2])
+
+    for infected_attribute in infected_list:
+        infected_id = int(infected_attribute[0])
+        infected_x = int(infected_attribute[1])
+        infected_y = int(infected_attribute[2])
+        if point_id == infected_id:
+            continue
+        dist = math.sqrt((infected_x - point_x)**2 + (infected_y - point_y)**2)
+        if dist <= 6:
+            output = output + "{}\n".format(infected_id)
+    
+    if output:
+        return output
+
+def load_infected_file(filename):
+    infected_file = open(filename, "r")
+    infected_lines = infected_file.read()
+    infected_file.close()
+
+    infected_lines_list = infected_lines.splitlines()
+    infected_list = []
+    for line in infected_lines_list:
+        infected_list.append(line.split(","))
+    
+    return infected_list
 
 # create Spark context with necessary configuration
 sc = SparkContext("local", "PySpark Word Count Exmaple")
 
 # read infected points from file and store in memory
-
+infected_list = load_infected_file("INFECTED-small.csv")
 
 # read data from text file and split each line into its normal point attributes
-points = sc.textFile("PEOPLE.csv").flatMap(lambda line: line.split(","))
+point_lines = sc.textFile("PEOPLE.csv").flatMap(lambda line: line.split("\n"))
 
 # for each normal point, check to see if its in range of any infected point
-point_map = points.map(
-        lambda attributes: map_points(attributes)
-    )
+point_map = point_lines.map(
+        lambda line: map_points(infected_list, line)
+).filter(
+    lambda line: line
+).flatMap(
+    lambda line: line.split("\n")
+).filter(
+    lambda line: line
+).map(
+    lambda infected_id: (infected_id, 1)
+)
+
+# count the number of occurrances of each infected id to get number of close contacts
+close_count = point_map.reduceByKey(lambda x,y:(x+y))
 
 # save the counts to output
-# wordCounts.saveAsTextFile("temp/")
+close_count.saveAsTextFile("temp/")
