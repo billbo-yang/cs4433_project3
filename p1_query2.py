67284584ee65b80f8611b456102057545b8ef224
Gus Teran <ateran@wpi.edu>
2022-02-24 23:08:32 -0500

    Complete query 2

--- p1_query2.py
+++ p1_query2.py
@@ -1,58 +1,109 @@
 import math
 from pyspark import SparkContext, SparkConf, sql
+from operator import add
 
+MAX_SIZE = 10000
 GRID_SIZE = 100
 COVID_DIST = 6
+ROWS = MAX_SIZE / GRID_SIZE
+
 
 def key_locations(x, y):
-    key0 = math.floor(x / GRID_SIZE) * math.floor(y / GRID_SIZE) + math.floor(x / GRID_SIZE)
-    key1 = math.floor((x + COVID_DIST) / GRID_SIZE) * math.floor((y + COVID_DIST) / GRID_SIZE) + math.floor((x + COVID_DIST) / GRID_SIZE)
-    key2 = math.floor((x + COVID_DIST) / GRID_SIZE) * math.floor((y - COVID_DIST) / GRID_SIZE) + math.floor((x + COVID_DIST) / GRID_SIZE)
-    key3 = math.floor((x - COVID_DIST) / GRID_SIZE) * math.floor((y + COVID_DIST) / GRID_SIZE) + math.floor((x - COVID_DIST) / GRID_SIZE)
-    key4 = math.floor((x - COVID_DIST) / GRID_SIZE) * math.floor((y - COVID_DIST) / GRID_SIZE) + math.floor((x - COVID_DIST) / GRID_SIZE)
+    key0 = int(ROWS * math.floor(y / GRID_SIZE) + math.floor(x / GRID_SIZE))
+    key1 = int(ROWS * math.floor((y - COVID_DIST) / GRID_SIZE) +
+               math.floor((x + COVID_DIST) / GRID_SIZE))
+    key2 = int(ROWS * math.floor((y + COVID_DIST) / GRID_SIZE) +
+               math.floor((x + COVID_DIST) / GRID_SIZE))
+    key3 = int(ROWS * math.floor((y + COVID_DIST) / GRID_SIZE) +
+               math.floor((x - COVID_DIST) / GRID_SIZE))
+    key4 = int(ROWS * math.floor((y - COVID_DIST) / GRID_SIZE) +
+               math.floor((x - COVID_DIST) / GRID_SIZE))
     return set({key0, key1, key2, key3, key4})
 
+
 def map_points(isInfected, line):
-    output = ""
+    output = []
 
     point_attributes = line.split(",")
     point_x = int(point_attributes[1])
     point_y = int(point_attributes[2])
+    infected_id = -1
+    if isInfected:
+        infected_id = int(point_attributes[0])
 
     for key in key_locations(point_x, point_y):
-        row = "{},{},{},{}".format(key, point_x, point_y, isInfected)
-        if not output:
-            output = row
+        row = "{},{},{},{}".format(key, point_x, point_y, infected_id)
+        output.append(row)
+
+    return output
+
+
+def reduce_infected(line):
+    # print("A")
+    # print(key)
+    grid = line[0]
+    people = line[1]
+    infected_list = []
+    person_list = []
+    pairs = []
+    for person in people:
+        if person[2] != -1:
+            infected_list.append(person[0:3])
         else:
-            output = output + "\n" + row
-    
-    if output:
-        return output
+            person_list.append(person[0:2])
+    for infected in infected_list:
+        infected_people = []
+        for person in person_list:
+            if math.sqrt((infected[0] - person[0])**2 + (infected[1] - person[1])**2) <= 6:
+                infected_people.append(person)
+        if len(infected_people) > 0:
+            pairs.append((infected[2], infected_people))
 
-# create Spark context with necessary configuration
+    return pairs
+
+
+# Creates Spark context.
 sc = SparkContext("local", "PySpark Problem 1 Query 2")
-spark = sql.SparkSession(sc)
 
-# read data from text file and split each line into its normal point attributes
-point_lines = sc.textFile("PEOPLE-small.csv").flatMap(lambda line: line.split("\n"))
-infected_lines = sc.textFile("INFECTED-large.csv").flatMap(lambda line: line.split("\n"))
+# Reads the data from each data file.
+point_lines = sc.textFile(
+    "PEOPLE.csv").flatMap(lambda line: line.split("\n"))
+infected_lines = sc.textFile(
+    "INFECTED-large.csv").flatMap(lambda line: line.split("\n"))
 
-# for each normal point, check to see if its in range of any infected point
+# Parses and cleans the data of the people data points.
 point_map = point_lines.map(
-        lambda line: map_points(False, line)
+    lambda line: map_points(False, line)
+).flatMap(
+    lambda lines: lines
 ).map(
-        lambda line: (int(line.split(",")[0]),int(line.split(",")[1]),int(line.split(",")[2]),line.split(",")[3])
+    lambda line: (int(line.split(",")[0]), [
+                  (int(line.split(",")[1]), int(line.split(",")[2]), int(line.split(",")[3]))])
 )
+
+# Parses and cleans the data of the infected people data points and stores the
+# infected id.
 infected_map = infected_lines.map(
-        lambda line: map_points(True, line)
+    lambda line: map_points(True, line)
+).flatMap(
+    lambda lines: lines
 ).map(
-        lambda line: (int(line.split(",")[0]),int(line.split(",")[1]),int(line.split(",")[2]),line.split(",")[3])
+    lambda line: (int(line.split(",")[0]), [
+                  (int(line.split(",")[1]), int(line.split(",")[2]), line.split(",")[3])])
 )
 
-people = spark.createDataFrame(point_map, schema=['grid', 'x', 'y','infected'])
-infected_people = spark.createDataFrame(infected_map, schema=['grid', 'x', 'y','infected'])
+# Combines the people and the infected people.
+all_people = sc.union([point_map, infected_map])
 
-all_people = people.join(infected_people, on='grid')
+# Use a map function to create the lists of people that 
+# are near an infected person. Uses the folded lists of all people 
+# that are in that grid cell.
+all_pairs = all_people.foldByKey([], add).map(
+    reduce_infected).flatMap(lambda lines: lines)
 
-out = all_people.rdd
-out.saveAsTextFile("temp2/")
+# Folds the lists of people by key of infected person.
+# Removes duplicates from the pairs.
+final_pairs = all_pairs.foldByKey([], add).map(
+    lambda pairs: (pairs[0], set(pairs[1])))
+
+final_pairs.saveAsTextFile("temp2/")
